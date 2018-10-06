04866b73e462104308b96e16c1c8141c3dc12d97
Danila <Danila00000@Gmail.com>
2018-10-06 11:53:45 +0700

    Load all my work

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const data = [
+  { id: 0, value: 'zero', title: 'choose a phrase' },
+  { id: 1, value: '1', title: 'get over her!' },
+  { id: 2, value: '2', title: 'finish him!' },
+  { id: 3, value: '3', title: 'your soup is mine!' },
+  { id: 4, value: '4', title: 'you a never win!' },
+  { id: 5, value: '5', title: 'fatality' },
+  { id: 5, value: '5', title: 'brutality' },
+]
